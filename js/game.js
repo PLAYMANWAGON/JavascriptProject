057b218e3b23ca5621ce354206c69bd02c926fbb
Aironas <aironas.keturka@elev.ga.ntig.se>
2021-02-12 09:33:48 +0100

    fixade onclick på player-name inputs

--- js/game.js
+++ js/game.js
@@ -4,15 +4,16 @@ var tttBoard = [
     [0, 0, 0]
 ];
 
+let gameSquare = document.querySelector("")
+
 
 function RegP1() {
-    var Player1 = document.getElementById("name1");
+    var Player1 = document.getElementById("name1").value;
     console.log(Player1);
 }
 
 function RegP2() {
-
-    var Player2 = document.getElementById('name2');
+    var Player2 = document.getElementById('name2').value;
     console.log(Player2);
 }
 
