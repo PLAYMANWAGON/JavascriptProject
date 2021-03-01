435f85272eb20e1e7e616249342bbfd4cd6e1790
Aironas <aironas.keturka@elev.ga.ntig.se>
2021-03-01 18:57:05 +0100

    ändrat text högra ruta+interaktiv bild på hund

--- game.js
+++ game.js
@@ -1,19 +1,1 @@
-var tttBoard = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
-
-//  let gameSquare = document.querySelector("")
-
-function RegP1() {
-    var Player1 = document.getElementById("name1").value;
-    console.log(Player1);
-}
-
-function RegP2() {
-    var Player2 = document.getElementById('name2').value;
-    console.log(Player2);
-}
-
 
