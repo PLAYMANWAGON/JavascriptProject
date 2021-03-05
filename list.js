2bb1a217595013b8ceabd6a9a0e4ed5a08fa49ef
Aironas <aironas.keturka@elev.ga.ntig.se>
2021-03-05 19:30:14 +0100

    inser att js är för svårt. har gjort bara simpl js

--- list.js
+++ list.js
@@ -1,3 +1,7 @@
+/*Får inte local storage att funka, vet ej varför.
+const LOCAL_STORAGE_STUFF = "app.text";
+*/
+
 function createList()
 {
     var ul = document.createElement("ul");
@@ -7,11 +11,15 @@ function createList()
         "inse att datorn klarar inte av det",
         "spela roblox och få en bluescreen",
     ];
+
     
     document.getElementById("list").appendChild(ul);
+
     stuff.forEach(createEach);
 
-    function createEach(element, index, arr) {
+
+    function createEach(element)
+    {
         var li = document.createElement("li");
         
         ul.appendChild(li);
@@ -21,18 +29,22 @@ function createList()
         li.innerHTML = li.innerHTML + element;
     }
 
-    function addList() 
-    {
-        var text = document.getElementById("text");
-        li.appendChild(document.createTextNode(text.value));
-        ul.appendChild(li);
-    }
 }
 
+function addList() 
+{
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    var text = document.getElementById("text");
+    li.appendChild(document.createTextNode(text.value));
+    ul.appendChild(li);
+    save();
+}
 
-
+/*som sagt ovan, funkar ej, ingen aning varför.
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_STUFF, JSON.stringify(text));
+}
+*/
 
 createList();
-
-
-
