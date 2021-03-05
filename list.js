c03c7b327ec48af6eb4b25edf8a8efc317a520f3
Aironas <aironas.keturka@elev.ga.ntig.se>
2021-03-05 16:18:53 +0100

    gjorde en js array som skapar en li för varje obj

--- list.js
+++ list.js
@@ -1,8 +1,38 @@
-function addList() 
+function createList()
 {
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
-    var text = document.getElementById("text");
-    li.appendChild(document.createTextNode(text.value));
-    ul.appendChild(li);
+    var ul = document.createElement("ul");
+
+    let stuff = [
+        "Spela tekken 7",
+        "inse att datorn klarar inte av det",
+        "spela roblox och få en bluescreen",
+    ];
+    
+    document.getElementById("list").appendChild(ul);
+    stuff.forEach(createEach);
+
+    function createEach(element, index, arr) {
+        var li = document.createElement("li");
+        
+        ul.appendChild(li);
+
+        t = document.createTextNode(element);
+
+        li.innerHTML = li.innerHTML + element;
+    }
+
+    function addList() 
+    {
+        var text = document.getElementById("text");
+        li.appendChild(document.createTextNode(text.value));
+        ul.appendChild(li);
+    }
 }
+
+
+
+
+createList();
+
+
+
